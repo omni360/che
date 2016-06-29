7b5519ef5bbcc9374d9ec3287edc1eb3cdad1650
Vladyslav Zhukovskii <vzhukovskii@codenvy.com>
2016-06-29 17:12:28 +0300

    Code clean up

--- ide/che-core-ide-app/src/main/java/org/eclipse/che/ide/projectimport/wizard/ProjectResolver.java
+++ ide/che-core-ide-app/src/main/java/org/eclipse/che/ide/projectimport/wizard/ProjectResolver.java
@@ -18,10 +18,6 @@ import org.eclipse.che.api.promises.client.Function;
 import org.eclipse.che.api.promises.client.FunctionException;
 import org.eclipse.che.api.promises.client.Promise;
 import org.eclipse.che.api.promises.client.PromiseProvider;
-import org.eclipse.che.ide.api.notification.Notification;
-import org.eclipse.che.ide.api.notification.NotificationListener;
-import org.eclipse.che.ide.api.notification.NotificationManager;
-import org.eclipse.che.ide.api.notification.StatusNotification;
 import org.eclipse.che.ide.api.project.MutableProjectConfig;
 import org.eclipse.che.ide.api.project.type.ProjectTypeRegistry;
 import org.eclipse.che.ide.api.resources.Project;
@@ -30,7 +26,6 @@ import org.eclipse.che.ide.projecttype.wizard.presenter.ProjectWizardPresenter;
 import java.util.List;
 
 import static com.google.common.collect.Lists.newArrayList;
-import static org.eclipse.che.ide.api.notification.StatusNotification.Status.SUCCESS;
 
 /**
  * The class contains business logic which allows resolve project type and call updater.
@@ -44,17 +39,14 @@ public class ProjectResolver {
     private final ProjectTypeRegistry    projectTypeRegistry;
     private final PromiseProvider        promiseProvider;
     private final ProjectWizardPresenter projectWizard;
-    private final NotificationManager    notificationManager;
 
     @Inject
     public ProjectResolver(ProjectTypeRegistry projectTypeRegistry,
                            PromiseProvider promiseProvider,
-                           ProjectWizardPresenter projectWizard,
-                           NotificationManager notificationManager) {
+                           ProjectWizardPresenter projectWizard) {
         this.projectTypeRegistry = projectTypeRegistry;
         this.promiseProvider = promiseProvider;
         this.projectWizard = projectWizard;
-        this.notificationManager = notificationManager;
     }
 
     public Promise<Project> resolve(final Project project) {
@@ -79,34 +71,7 @@ public class ProjectResolver {
                 } else if (primeTypes.size() == 1) {
                     config.setType(primeTypes.get(0));
                 } else {
-                    final NotificationListener notificationListener = new NotificationListener() {
-                        boolean clicked = false;
-
-                        @Override
-                        public void onClick(Notification notification) {
-                            if (!clicked) {
-                                projectWizard.show(config);
-                                clicked = true;
-                                notification.setListener(null);
-                                notification.setContent("");
-                            }
-                        }
-
-                        @Override
-                        public void onDoubleClick(Notification notification) {
-                            //stub
-                        }
-
-                        @Override
-                        public void onClose(Notification notification) {
-                            //stub
-                        }
-                    };
-
-                    notificationManager.notify("Project " + project.getName() + " has to be configured",
-                                               "Click here to set up your project.",
-                                               SUCCESS, StatusNotification.DisplayMode.FLOAT_MODE, notificationListener);
-
+                    projectWizard.show(config);
 
                     return promiseProvider.resolve(project);
                 }
